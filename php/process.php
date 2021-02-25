9b1e1e0acfe69aa47a9331948ee43629416194b9
Moises Nieto <enieto19@hotmail.com>
2021-02-26 00:47:21 +0100

    insert php

---
+++ php/process.php
@@ -0,0 +1,14 @@
+<?php 
+echo	$name = $_POST['name'];
+echo    $email = $_POST['email'];
+echo	$mensaje = $_POST['message'];
+
+//here header and value for the form
+$cabeceras .= $email. "\r\n";
+
+	if(mail('moi34_5@hotmail.com', $name, $mensaje,$cabeceras)){
+		echo "email sent";
+	}else{
+		echo "there an error!";
+	}
+ ?>
