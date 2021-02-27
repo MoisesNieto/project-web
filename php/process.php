c82bcfb99eafe2631ef21dd29e4dade22a70f674
Moises Nieto <enieto19@hotmail.com>
2021-02-27 17:40:01 +0100

    beta 1.4

--- php/process.php
+++ php/process.php
@@ -1,4 +1,9 @@
 <?php 
+
+
+header("Location:../index.html");
+die();
+
 echo	$name = $_POST['name'];
 echo    $email = $_POST['email'];
 echo	$mensaje = $_POST['message'];
@@ -6,9 +11,11 @@ echo	$mensaje = $_POST['message'];
 //here header and value for the form
 $cabeceras .= $email. "\r\n";
 
-	if(mail('moi34_5@hotmail.com', $name, $mensaje,$cabeceras)){
-		echo "email sent";
-	}else{
-		echo "there an error!";
-	}
+if(mail('moi34_5@hotmail.com', $name, $mensaje,$cabeceras)){
+	echo "correo enviado";
+}else{
+	echo "there an error!";
+}
+
+
  ?>
