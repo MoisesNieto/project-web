217d7af36c8b8a78a5966e43e9db2d6446d10ecb
Moises Nieto <enieto19@hotmail.com>
2021-03-18 00:14:19 +0100

    solution menu 1.0

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,22 @@
+
+const service = document.querySelector('#service');
+const tecnologic = document.querySelector('#tecnologia');
+const project = document.querySelector('#project');
+const contact = document.querySelector('#contact');
+const menu_items = document.querySelector('.menu_items');
+
+
 addEventListener ('DOMContentLoaded', () =>{
     const btn_menu = document.querySelector('.btn_menu');
     if(btn_menu){
         btn_menu.addEventListener('click', ()=>{
-            const menu_items = document.querySelector('.menu_items');
+           
             menu_items.classList.toggle('show');
-
         })
     }
+    service.addEventListener('click', ()=>{
+        menu_items.classList.remove('show');
+    });
+   
+
 })
