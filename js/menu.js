71ec2cea9dbb0fd8b5670df18c248fa545a4e97f
Moises Nieto <enieto19@hotmail.com>
2021-03-19 20:16:37 +0100

    beta 1.6

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,22 @@
 
-const service = document.querySelector('#service');
-const tecnologic = document.querySelector('#tecnologia');
-const project = document.querySelector('#project');
-const contact = document.querySelector('#contact');
 const menu_items = document.querySelector('.menu_items');
+document.addEventListener('DOMContentLoaded', () =>{
 
-
-addEventListener ('DOMContentLoaded', () =>{
     const btn_menu = document.querySelector('.btn_menu');
     if(btn_menu){
         btn_menu.addEventListener('click', ()=>{
-           
             menu_items.classList.toggle('show');
         })
-    }
-    service.addEventListener('click', ()=>{
-        menu_items.classList.remove('show');
-    });
-   
-
+    } 
 })
+document.addEventListener('click', (e)=>{
+        if(e.target.id === '#menu_ser' || e.target.id === '#menu_tec' 
+                || e.target.id === '#menu_pro' || e.target.id === '#menu_cant' ){
+            menu_items.classList.remove('show');
+        }
+})
+
+    
+
+
+
